30b663afe1245c0ed967a3d6748eb9b1e0cdb6d2
Jacek <s4253@pjwstk.edu.pl>
2024-03-02 21:30:27 +0100

    Update Hello World rest api

--- src/main/java/com/example/helloworld/rest/HelloWorld.java
+++ src/main/java/com/example/helloworld/rest/HelloWorld.java
@@ -1,8 +1,12 @@
 package com.example.helloworld.rest;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 @RestController
@@ -12,5 +16,17 @@ public class HelloWorld {
     public String helloWorld() {
         return "Hello world ! This is spring boot demo app ! (app in git)";
     }
+
+    @GetMapping("/name")
+    public String helloWorldWithParam(@PathVariable String name) {
+        return "Hello world ! My name is :  " + name;
+    }
+
+    @PostMapping("/postName")
+    public ResponseEntity<Object> postName(@PathVariable String name) {
+
+        return ResponseEntity.created(UriComponentsBuilder.fromHttpUrl("http://abc").build().toUri()).build();
+    }
+
 
 }
